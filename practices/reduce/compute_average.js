05796d3b5049eeea82a8f88e14db5d5adc1f2299
zr577378948 <577378948@qq.com>
2017-05-06 12:00:03 +0800

    four

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function compute_average(collection) {
-  //在这里写入代码
+  var result = 0;
+
+  for (var i in collection) {
+    result += collection[i];
+
+  }
+
+  return result / collection.length;
 }
 
 module.exports = compute_average;
