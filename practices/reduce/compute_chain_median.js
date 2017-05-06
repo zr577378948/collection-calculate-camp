382fb1701aeacbb7a06e4a871812c8ca5d9f4300
zr577378948 <577378948@qq.com>
2017-05-06 12:24:42 +0800

    four

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function compute_chain_median(collection) {
-  //在这里写入代码
+  var result=0;
+  collection =collection.split("->").sort(function(a,b){return a-b});
+  for(var i in collection){
+    result=(parseInt(collection[collection.length/2])+
+            parseInt(collection[collection.length/2-1]))/2;
+  }
+  return result;
 }
 
 module.exports = compute_chain_median;
