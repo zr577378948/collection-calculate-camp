a3f756ec9c2a8f222ede409844a419ecd5f9dfc1
zr577378948 <577378948@qq.com>
2017-05-06 11:33:07 +0800

    three commit

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,15 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+var number_map_to_word = function (collection) {
+
+  var result = [];
+  var word = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  for (var i in collection) {
+    result.push(word[collection[i]]);
+  }
+
+  return result;
 };
 
 module.exports = number_map_to_word;
--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,14 +1,14 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
+var number_map_to_word_over_26 = function (collection) {
 
   var result = [];
   var word = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  for(var i in collection){
-    if(parseInt(collection[i]/26)>0){
-      result.push(word[1]+word[collection[i]%26]);
-    }else{
+  for (var i in collection) {
+    if (parseInt(collection[i] / 26) > 0) {
+      result.push(word[1] + word[collection[i] % 26]);
+    } else {
       result.push(word[collection[i]]);
     }
   }
