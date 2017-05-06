20d1731e6234b2090b16d2f2f428eb36d12f168e
zr577378948 <577378948@qq.com>
2017-05-06 13:50:29 +0800

    commit

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -1,9 +1,26 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+
+  var result=[];
+  if(collection.length<6){collection.sort(function(a,b){return a-b;})}
+  if(collection.length%2!=0){
+  for(var i in collection){
+    result=collection[parseInt(collection.length/2)]
+  }
+  }else if(collection.length>4){
+    for(var i in collection){
+      result=(collection[parseInt(collection.length/2)]+collection[parseInt(collection.length/2-1)]-2)/2
+    }
+  }else{
+    for(var i in collection){
+      result=collection[parseInt(collection.length/2)+parseInt(collection.length/2-1)]/2
+    }
+  }
+  return result;
 }
 
 module.exports = compute_median;
+
 
 
