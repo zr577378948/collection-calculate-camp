aef4f7fb1933b3248a3d0415b2685409d9436974
zr577378948 <577378948@qq.com>
2017-05-16 17:30:23 +0800

    practice

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,10 +1,12 @@
 function collect_same_elements(collection_a, object_b) {
 
   var result = [];
+
   for (var i in collection_a) {
     for (var j in object_b.value) {
-      result.push(object_b.value[j]);
-
+      if (collection_a[i] === object_b.value[j]) {
+        result.push(object_b.value[j]);
+      }
     }
   }
   return result;
--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+
+  var resultArray = [];
+
+  for (var j in collection_a) {
+    for (var i in object_b.value) {
+      if (object_b.value[i] === collection_a[j].key) {
+        resultArray.push(collection_a[j].key)
+      }
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = collect_same_elements;
